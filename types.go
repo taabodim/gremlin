1ae74d9509f009bae33c1912cb7d6e5e088b7fe5
Evan Marcey <emarcey@cbinsights.com>
2019-05-13 11:40:18 -0400

    Compare vertexes

--- types.go
+++ types.go
@@ -19,15 +19,45 @@ type Vertex struct {
 	Value VertexValue `json:"@value"`
 }
 
+func (v1 Vertex) Equals(v2 Vertex) bool {
+	return v1.Type == v2.Type && v1.Value.Equals(v2.Value)
+}
+
 type VertexValue struct {
 	ID         string                      `json:"id"`
 	Label      string                      `json:"label"`
 	Properties map[string][]VertexProperty `json:"properties"`
 }
 
+func (v1 VertexValue) Equals(v2 VertexValue) bool {
+	if v1.ID != v2.ID || v1.Label != v2.Label {
+		return false
+	}
+	if len(v1.Properties) != len(v2.Properties) {
+		return false
+	}
+	for k, val1Slice := range v1.Properties {
+		val2Slice, ok := v2.Properties[k]
+		if !ok || len(val1Slice) != len(val2Slice) {
+			return false
+		}
+		for i, val1 := range val1Slice {
+			val2 := val2Slice[i]
+			if !val1.Equals(val2) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 type VertexProperty struct {
 	Type  string              `json:"@type"`
 	Value VertexPropertyValue `json:"@value"`
+}
+
+func (v1 VertexProperty) Equals(v2 VertexProperty) bool {
+	return v1.Type == v2.Type && v1.Value.Equals(v2.Value)
 }
 
 type EdgeProperty struct {
@@ -41,6 +71,10 @@ type VertexPropertyValue struct {
 	Value interface{}  `json:"value"`
 }
 
+func (v1 VertexPropertyValue) Equals(v2 VertexPropertyValue) bool {
+	return v1.ID.Equals(v2.ID) && v1.Label == v2.Label && InterfacesMatch(v1.Value, v2.Value)
+}
+
 type EdgePropertyValue struct {
 	Label string      `json:"key"`
 	Value interface{} `json:"value"`
@@ -51,6 +85,10 @@ type GenericValues []GenericValue
 type GenericValue struct {
 	Type  string      `json:"@type"`
 	Value interface{} `json:"@value"`
+}
+
+func (g1 GenericValue) Equals(g2 GenericValue) bool {
+	return g1.Type == g2.Type && InterfacesMatch(g1.Value, g2.Value)
 }
 
 type Edges []Edge
